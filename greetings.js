906239f4f91026db3dfc94b797a6dc90869f6b13
rafsan102 <ahmedrafsan101@gmail.com>
2026-07-31 22:07:02 +0600

    implement fs

---
+++ greetings.js
@@ -0,0 +1,24 @@
+const args = process.argv;
+
+//process.arg[0]= node path
+//process.arg[1]= file path
+//process.arg[2]= first actual arguments
+
+const name = args[2] || 'guest';
+const time = new Date().getHours();
+const timeMinute = new Date().getMinutes();
+const timeSecond = new Date().getSeconds();
+
+let greetings;
+
+if (time < 12) {
+  greetings= 'good morning'
+} else if (time < 18) {
+  greetings= 'good afternoon'
+} else {
+  greetings= 'good evening'
+}
+
+console.log(`${greetings} ${name}`);
+
+console.log(`${time} :${timeMinute} : ${timeSecond}`);
