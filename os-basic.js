35118c187cc140caee45d6a8d7798be81176dea9
rafsan102 <ahmedrafsan101@gmail.com>
2026-08-07 23:42:50 +0600

    check os basic

--- os-basic.js
+++ os-basic.js
@@ -1,3 +1,21 @@
 const os = require('os');
 
 console.log('System info \n');
+
+console.log('-'.repeat(30));
+
+console.log('platform Details:');
+
+console.log('Platform:',os.platform());
+console.log('Architecture:',os.arch());
+console.log('Os type:',os.type());
+console.log('Os Release:',os.release());
+console.log('Hostname:', os.hostname());
+console.log('CPU Info:');
+
+const cpus = os.cpus();
+
+console.log('CPU model:', cpus[0].model);
+console.log('Number of cores:',cpus.length);
+console.log('CPU speed', cpus[0].speed);
+console.log('CPU speed', cpus[0]);
