5de1e677cd479cb87eca174a7786a5bc9cc08905
rafsan102 <ahmedrafsan101@gmail.com>
2026-08-11 23:29:33 +0600

    doing encryption method using crypto cipher module

---
+++ encrypsion.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+let encrypted = cipher.update('Hello Rafsan', 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+console.log(encrypted);
--- hashing.js
+++ hashing.js
@@ -2,4 +2,8 @@
 
 const crypto = require('crypto');
 
-console.log('');
+console.log('\n MD5 hash:');
+const md5hash = crypto.createHash('md5').update('password123').digest('hex');
+
+console.log('input: password123');
+console.log('Md5 hashPassword',md5hash);
